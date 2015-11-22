fa71efc523d45a318ddb0665626b5f48c2262f09
Grigory Malivenko <nerox8664@gmail.com>
2015-11-22 20:29:23 +0300

    Insert sort implemented

--- src/lib/sort.js
+++ src/lib/sort.js
@@ -12,13 +12,31 @@ function swapElements(array, i, j) {
 
 function bubbleSort(array) {
   var t, i, j, len = array.length;
-  for(i = 0; i < len; i++) {
-    for(j = 0; j < len; j++) {
+  for (i = 0; i < len; i++) {
+    for (j = 0; j < len; j++) {
       if (array[j] < array[j + 1]) {
         swapElements(array, j, j + 1);
       }
     }
   }
+
+  return array;
+}
+
+function insertSort(array) {
+  var i, j, len = array.length;
+  for (i = 1; i < len; i++) {
+    var key = array[i];
+    for (j = i - 1; j >= 0; j--) {
+      if (array[j] > key) {
+        break;
+      }
+
+      array[j + 1] = array[j];
+      array[j] = key;
+    }
+  }
+
   return array;
 }
 
@@ -40,9 +58,9 @@ function quickSort(array, medianSelector) {
     var median = array[medianIndex];
     var i = from, j = to;
 
-    while(i < j) {
-      while(array[i] > median) i++;
-      while(array[j] < median) j--;
+    while (i < j) {
+      while (array[i] > median) i++;
+      while (array[j] < median) j--;
       if (i < j) {
         swapElements(array, i, j);
         i++;
@@ -63,13 +81,16 @@ module.exports = {
   quickSortAverageMedian: function(array) {
     return quickSort(array, randomMedianSelector);
   },
+
   quickSortRandomMedian: function(array) {
     return quickSort(array, randomMedianSelector);
   },
+
   quickSortFixedMedian: function(array) {
     return quickSort(array, fixedMedianSelector);
   },
+
   shellsSort: bubbleSort,
   selectionSort: bubbleSort,
-  insertionSort: bubbleSort,
+  insertionSort: insertSort,
 };
